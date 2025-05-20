d4196a9c771050e9994b24c64a52fc5efc4c8fd6
Crisgmz <94495044+Crisgmz@users.noreply.github.com>
2025-05-19 23:34:27 -0400

    Update MouseTracker.tsx

--- src/components/MouseTracker.tsx
+++ src/components/MouseTracker.tsx
@@ -1,30 +1,31 @@
 
+// src/components/MouseTracker.tsx
 import { useState, useEffect } from "react";
 
 const MouseTracker = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  
-  // Mouse tracking for custom cursor
+
   useEffect(() => {
     const mouseMoveHandler = (event: MouseEvent) => {
       setMousePosition({ x: event.clientX, y: event.clientY });
     };
 
     window.addEventListener("mousemove", mouseMoveHandler);
-
-    return () => {
-      window.removeEventListener("mousemove", mouseMoveHandler);
-    };
+    return () => window.removeEventListener("mousemove", mouseMoveHandler);
   }, []);
 
   return (
-    <div 
-      className="cursor-outline z-50"
-      style={{ 
-        left: `${mousePosition.x}px`,
-        top: `${mousePosition.y}px`,
+    <div
+      className="cursor-outline"
+      style={{
+        position: "fixed",
+        top: mousePosition.y,
+        left: mousePosition.x,
+        transform: "translate(-50%, -50%)",
+        pointerEvents: "none",
+        zIndex: 9999,
       }}
-    ></div>
+    />
   );
 };
 
